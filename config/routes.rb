442c1b2ba3c2d076e0f4b1f29b27fa9eee1d4fb5
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2022-01-21 15:38:59 -0500

    created partials

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'bienvenida', to: 'home#index'
 
   get 'articles/new', to: 'articles#new' 
-  get 'articles/2', to: 'articles#show'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+
+  patch 'articles/:id', to: 'articles#update', as: :article
   post 'articles', to: 'articles#create'
 end
