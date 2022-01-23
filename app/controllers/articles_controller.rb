3b2fef1635a724719acf62c34c1e475e1ba3c3e9
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2022-01-23 07:35:47 -0500

    updating articles, index, new, show, index and routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,16 @@
 class ArticlesController < ApplicationController
 
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
   def show
-    @article = Article.find(params[:id])
   end
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], content: params[:article][:content])
 
     redirect_to @article
@@ -20,5 +22,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.create(title: params[:article][:title], content: params[:article][:content])
     render json: @article
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
