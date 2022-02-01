6a90353625d2c234223115fae43ce14fa13f1f52
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2022-01-31 20:52:15 -0500

    created categories model with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,19 @@ Rails.application.routes.draw do
   get 'bienvenida', to: 'home#index'
 
   root to: 'home#index'
-
+  resources :categories
   get 'articles/user/:user_id', to: 'articles#from_author'
 
-  get 'articles', to:'articles#index'
-  get 'articles/new', to: 'articles#new', as: :new_article
-  get 'articles/:id', to: 'articles#show'
-  get 'articles/:id/edit', to: 'articles#edit'
+  resources :articles do
+    get 'user/user_id', to: 'articles#from_author', on: :collection
+  end
 
-  patch 'articles/:id', to: 'articles#update', as: :article
-  post 'articles', to: 'articles#create'
-  delete 'articles/:id', to:'articles#destroy'
+  # get 'articles', to:'articles#index'
+  # get 'articles/new', to: 'articles#new', as: :new_article
+  # get 'articles/:id', to: 'articles#show'
+  # get 'articles/:id/edit', to: 'articles#edit'
+
+  # patch 'articles/:id', to: 'articles#update', as: :article
+  # post 'articles', to: 'articles#create'
+  # delete 'articles/:id', to:'articles#destroy'
 end
